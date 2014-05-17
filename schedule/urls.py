4527b2a97eb89c207d5f8a0a9838850541733a69
Daniel Davis <daniel.davis@excella.com>
2014-05-17 12:33:16 -0400

    Updated time_away_form to use the content block inside of base collab view, updated urls and views to fix routing to Calendar

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
-from .views import TimeAwayView
+from .views import TimeAwayView, CalendarView
 
 urlpatterns = patterns('schedule.views',
                        url(r'^time_away$', TimeAwayView.as_view(), name='time_away'),
-                       
+                       url(r'^calendar$', CalendarView.as_view(), name='calendar')
                        )
--- schedule/views.py
+++ schedule/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 from .models import time_away
 
 class TimeAwayView(CreateView):
 	model = time_away
+
+class CalendarView(TemplateView):
+	template_name = "calendar.html"

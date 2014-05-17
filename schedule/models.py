3ea27a987bce27b85cc3ed2f3638ff9554dc5da7
thegrtman <kgroat@live.com>
2014-05-17 15:40:16 -0400

    Created model for TimeAwayType and linked with TimeAway model

--- schedule/models.py
+++ schedule/models.py
@@ -2,8 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
+class TimeAwayType(models.Model):
+	code = models.CharField(max_length=30)
+
 class TimeAway(models.Model):
 	user = models.ForeignKey(User)
 	date = models.DateField()
+	type = models.ForeignKey(TimeAwayType)
 	def get_absolute_url(self):
 		return "time_away"
--- schedule/tests.py
+++ schedule/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from .models import TimeAwayType
 
 class ScheduleTest(TestCase):
 
@@ -14,3 +15,13 @@ class ScheduleTest(TestCase):
     def test_vacation_list_invalid_user(self):
         response = self.client.get(reverse('schedule:list'))
         self.assertEquals(404, response.status_code)
+
+    def test_timeAwayType_list_returns_all(self):
+        # Arrange
+		a = TimeAwayType.objects.create(code='Away')
+		
+		# Act
+		result = TimeAwayType.objects.all()
+		
+		# Assert
+		self.assertIn(a, result)

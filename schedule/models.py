f3e425594761ffb1b7965106dda9c0cb82195cf7
Daniel Davis <daniel.davis@excella.com>
2014-05-17 13:14:18 -0400

    fixed url routing

--- schedule/models.py
+++ schedule/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 class TimeAway(models.Model):
 	user = models.ForeignKey(User)
 	date = models.DateField()
 	def get_absolute_url(self):
-		return "/schedule/time_away"
+		return "time_away"

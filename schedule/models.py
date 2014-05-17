3d2d360b4d23bee4712128c8e2d230276a5eab4a
thegrtman <kgroat@live.com>
2014-05-17 16:25:53 -0400

    Updates to models to remove TimeAwayType

--- schedule/models.py
+++ schedule/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
-class TimeAwayType(models.Model):
-	code = models.CharField(max_length=30)
-
 class TimeAway(models.Model):
+	TIME_AWAY_TYPES = (
+		('AWA', 'Alternate Work Arrangement'),
+		('AWAY', 'Away')
+	)
 	user = models.ForeignKey(User)
 	date = models.DateField()
-	type = models.ForeignKey(TimeAwayType, null=True, blank=True)
+	type = models.CharField(max_length=4, choices=TIME_AWAY_TYPES, default='AWAY')
 	def get_absolute_url(self):
 		return "time_away"
